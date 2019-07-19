91452c999b95ad420dfb9c0bbed17d54b3a3b5f3
Carlos <carlosgomezsoza@gmail.com>
2019-07-18 22:24:38 -0600

    pages for organo federado and consejo

--- gatsby-node.js
+++ gatsby-node.js
@@ -27,18 +27,34 @@ const makeRequest = (graphql, request) => new Promise((resolve, reject) => {
 exports.createPages = ({ actions, graphql }) => {
     const { createPage } = actions;
     
+	
+	//create pages for three types that require static page
     const getAsociaciones = makeRequest(graphql, `
       {
-        allStrapiAsociacion {
-          edges {
-            node {
-              id
-            }
-          }
-        }
-      }
+		  allStrapiAsociacion {
+			edges {
+			  node {
+				id
+			  }
+			}
+		  }
+		  allStrapiOrganofederado {
+			edges {
+			  node {
+				id
+			  }
+			}
+		  }
+		  allStrapiConsejo {
+			edges {
+			  node {
+				id
+			  }
+			}
+		  }
+		}
       `).then(result => {
-      // Create pages for each article.
+      // Create pages for each asociation
       result.data.allStrapiAsociacion.edges.forEach(({ node }) => {
         createPage({
           path: `/${node.id}`,
@@ -48,7 +64,31 @@ exports.createPages = ({ actions, graphql }) => {
           },
         })
       })
-    });
+	});
+	
+	// Create pages for each organo federado
+      result.data.allStrapiOrganofederado.edges.forEach(({ node }) => {
+        createPage({
+          path: `/${node.id}`,
+          component: path.resolve(`src/components/asociacion/asociacion.js`),
+          context: {
+            id: node.id,
+          },
+        })
+      })
+	});
+	
+	// Create pages for each consejo
+      result.data.allStrapiAsociacion.edges.forEach(({ node }) => {
+        createPage({
+          path: `/${node.id}`,
+          component: path.resolve(`src/components/asociacion/asociacion.js`),
+          context: {
+            id: node.id,
+          },
+        })
+      })
+	});
     
     // Query for articles nodes to use in creating pages.
     return getAsociaciones;
