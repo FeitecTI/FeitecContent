a4a2e9ea24bfc6387cb2ea6b6d38601a49071307
Esteban <esteban1113001@gmail.com>
2019-07-19 21:58:34 -0600

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
+import Layout from "../components/op/layout"
 import SEO from "../components/seo"
 //import DeleteList from "../components/deleteList"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Feitec" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-
+      <p>JAJA SALU3 Solo estoy rellenando para que no se despiche tere ok bai</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
