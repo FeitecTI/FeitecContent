72db2d077796d7bb202afbbcbf4d2a10e8485591
Esteban <esteban1113001@gmail.com>
2019-08-07 22:26:44 -0600

    redesign

--- src/components/layout/dropdown.js
+++ src/components/layout/dropdown.js
@@ -1,25 +1,26 @@
 import React from "react"
-import 
-{
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu
-} from "reactstrap"
+import { UncontrolledDropdown, DropdownToggle, DropdownMenu } from "reactstrap"
 import { Link } from "gatsby"
 
 const Dropdown = ({ text, pathsInfo }) => (
-    <UncontrolledDropdown nav inNavbar>
-        <DropdownToggle nav caret className="text-dark">
-            {text}
-        </DropdownToggle>
-        <DropdownMenu>
-            {pathsInfo.map((info, index) => (
-                <Link to={info.path} key={index} className="dropdown-item text-dark" style={{ whiteSpace: "pre-line" }}>
-                    {info.text}
-                </Link>
-            ))}
-        </DropdownMenu>
-    </UncontrolledDropdown>
+  <UncontrolledDropdown nav inNavbar>
+    <DropdownToggle nav caret className="text-dark">
+      {text}
+    </DropdownToggle>
+    <DropdownMenu>
+      {pathsInfo.map((info, index) => (
+        <Link
+          to={info.path}
+          key={index}
+          className="dropdown-item text-dark"
+          style={{ whiteSpace: "pre-line" }}
+          activeClassName="active"
+        >
+          {info.text}
+        </Link>
+      ))}
+    </DropdownMenu>
+  </UncontrolledDropdown>
 )
 
 export default Dropdown
--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Navbar, NavbarToggler, Nav, Collapse } from "reactstrap"
-import { HEADER, MAIN_COLOR } from "../../config/consts"
+import { Navbar, NavbarToggler, Nav, Collapse, Media } from "reactstrap"
+
+import { HEADER } from "../../config/consts"
 import Dropdown from "./dropdown"
 import Item from "./item"
 import pathInfo from "../../config/pathInfo"
+import logo from "../../images/Logo-Feitec.png"
 
 class Header extends React.Component {
   constructor(props) {
@@ -23,13 +25,13 @@ class Header extends React.Component {
   render() {
     // Carpeta Layout (Header, Footer, Layout)
     return (
-      <Navbar light style={{ backgroundColor: MAIN_COLOR }} expand="md">
+      <Navbar light expand="md">
         <Link to="/" className="navbar-brand mr-auto text-dark">
-          {this.props.siteTitle}
+          <Media src={logo} style={{ maxHeight: "3rem" }} />
         </Link>
         <NavbarToggler onClick={this.toggleNavbar} className="ml-2" />
         <Collapse isOpen={!this.state.collapsed} navbar>
-          <Nav className="ml-auto" navbar>
+          <Nav className="ml-auto" tabs>
             {HEADER.map((info, index) => {
               if (info instanceof pathInfo) {
                 return <Item path={info.path} text={info.text} key={index} />
@@ -52,3 +54,15 @@ class Header extends React.Component {
 }
 
 export default Header
+
+export const query = graphql`
+  query {
+    fileName: file(relativePath: { eq: "images/Logo-Feitec.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 400, maxHeight: 250) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
