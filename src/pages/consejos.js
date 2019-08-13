50c47a9002e3db9b50df70e50349e14c6741766f
Josue <josue.prendas8@gmail.com>
2019-08-12 22:10:08 -0600

    List Consejos-Organos

--- src/pages/consejos.js
+++ src/pages/consejos.js
@@ -1,35 +1,48 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
-
+import { graphql } from "gatsby"
+import Contenedor from "../components/Items/consejos-organos/contenedorCons-Org"
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
-//import DeleteList from "../components/deleteList"
 
-const Consejos = ({data}) => {
-  return(
+function Consejos({ data }) {
+  return (
     <Layout>
       <SEO title="Feitec" />
-      <ul>
-        {data.allStrapiConsejo.edges.map(document => (
-          <li key={document.node.id}>
-            <Link to = {`/${document.node.id}`}>{document.node.nombre}</Link>
-          </li>
+      <div className="row">
+        {data.allStrapiConsejo.edges.map((document, index) => (
+          <div className="col-md-6 my-3 shadow container">
+            <Contenedor
+              data={{
+                id: document.node.id,
+                src: document.node.foto.childImageSharp.fixed.src,
+                nombre: document.node.nombre,
+              }}
+              key={index}
+            />
+          </div>
         ))}
-      </ul>
-      <Link to="/page-2/">Go to page 2</Link>
+      </div>
     </Layout>
   )
 }
+
 export default Consejos
 
-
-export const pageQuery = graphql`  
+export const pageQuery = graphql`
   query ConsejosQuery {
     allStrapiConsejo {
       edges {
         node {
           id
           nombre
+          foto {
+            id
+            childImageSharp {
+              fixed(height: 300, width: 550, quality: 100) {
+                src
+              }
+            }
+          }
         }
       }
     }
