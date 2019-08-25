0ca6be7d383efce6ce1b2487cc1aafae6ce5fd1c
Esteban <esteban1113001@gmail.com>
2019-08-25 02:00:29 -0600

    MainHeader

--- src/components/layout/MainHeader.js
+++ src/components/layout/MainHeader.js
@@ -8,7 +8,7 @@ import Item from "./item"
 import pathInfo from "../../config/pathInfo"
 import logo from "../../images/Logo-Feitec.png"
 
-class Header extends React.Component {
+class MainHeader extends React.Component {
   constructor(props) {
     super(props)
     this.toggleNavbar = this.toggleNavbar.bind(this)
@@ -29,23 +29,39 @@ class Header extends React.Component {
         expand="md"
         style={{ backgroundColor: "#eeeeee", maxHeight: "5rem" }}
       >
-        <Link to="/" className="navbar-brand mr-auto text-dark">
-          <Media src={logo} style={{ maxHeight: "3rem", margin: "0 auto" }} />
-        </Link>
         <NavbarToggler onClick={this.toggleNavbar} className="ml-2" />
-        <Collapse isOpen={!this.state.collapsed} navbar>
-          <Nav className="ml-auto" navbar>
+        <Collapse navbar isOpen={!this.state.collapsed}>
+          <Nav navbar style={{ margin: "auto" }}>
             {HEADER.map((info, index) => {
+              let icon = <></>
+              if (index === HEADER.length / 2) {
+                icon = (
+                  <Link to="/" className="navbar-brand mr-auto">
+                    <Media
+                      src={logo}
+                      style={{ maxHeight: "3rem", margin: "auto" }}
+                      key={index + 10}
+                    />
+                  </Link>
+                )
+              }
               if (info instanceof pathInfo) {
-                return <Item path={info.path} text={info.text} key={index} />
-              } else {
-                // Tipo dropdownInfo
                 return (
-                  <Dropdown
-                    text={info.name}
-                    pathsInfo={info.pathInfo}
-                    key={index}
-                  />
+                  <>
+                    {icon}
+                    <Item path={info.path} text={info.text} key={index} />
+                  </>
+                )
+              } else {
+                return (
+                  <>
+                    {icon}
+                    <Dropdown
+                      text={info.name}
+                      pathsInfo={info.pathInfo}
+                      key={index}
+                    />
+                  </>
                 )
               }
             })}
@@ -56,4 +72,4 @@ class Header extends React.Component {
   }
 }
 
-export default Header
+export default MainHeader
