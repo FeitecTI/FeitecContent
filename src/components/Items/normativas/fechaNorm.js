543b76a53297f4f35109915af8c891502e21d832
Esteban <esteban1113001@gmail.com>
2019-09-15 19:18:05 -0600

    Normativas rediseñadas

--- src/components/Items/normativas/fechaNorm.js
+++ src/components/Items/normativas/fechaNorm.js
@@ -1,25 +1,8 @@
 import React from "react"
 import "./styleNorm.css"
 
-const Fecha = ({ data }) => {
-  if (data != null) {
-    return (
-      <div>
-        <h5 className="texto" style={{ opacity: "0.7" }}>
-          {" "}
-          {data.fecha}{" "}
-        </h5>
-      </div>
-    )
-  } else {
-    return (
-      <div>
-        <h5 className="texto" style={{ opacity: "0.7" }}>
-          {" "}
-          -{" "}
-        </h5>
-      </div>
-    )
-  }
-}
+const Fecha = ({ data }) => (
+  <div style={{ opacity: "0.7", fontSize: "0.8rem" }}>{data.fecha}</div>
+)
+
 export default Fecha
