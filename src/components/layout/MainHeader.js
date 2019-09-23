6c0c9e3f658d23b8ba83f4627557788bbd9289e7
Esteban <esteban1113001@gmail.com>
2019-09-22 20:31:14 -0600

    Fixed Mobile Navbar and added animation on MainHeader

--- src/components/layout/MainHeader.js
+++ src/components/layout/MainHeader.js
@@ -12,8 +12,38 @@ class MainHeader extends React.Component {
   constructor(props) {
     super(props)
     this.toggleNavbar = this.toggleNavbar.bind(this)
+    this.handleScroll = this.handleScroll.bind(this)
     this.state = {
       collapsed: true,
+      maxLogoSize: 5,
+      lastScrollTop: 0,
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll)
+  }
+
+  handleScroll(event) {
+    let scrollTop = window.pageYOffset
+    const changeAmount = 0.05
+    if (scrollTop > this.state.lastScrollTop && this.state.maxLogoSize > 3) {
+      this.setState({
+        maxLogoSize: this.state.maxLogoSize - changeAmount,
+        lastScrollTop: scrollTop,
+      })
+    } else if (
+      this.state.maxLogoSize < 5 &&
+      this.state.lastScrollTop > scrollTop
+    ) {
+      this.setState({
+        maxLogoSize: this.state.maxLogoSize + changeAmount,
+        lastScrollTop: scrollTop,
+      })
     }
   }
 
@@ -22,24 +52,28 @@ class MainHeader extends React.Component {
       collapsed: !this.state.collapsed,
     })
   }
+
   render() {
     return (
-      <Navbar
-        light
-        expand="md"
-        style={{ backgroundColor: "#eeeeee", maxHeight: "5rem" }}
-      >
+      <Navbar light expand="md" style={{ backgroundColor: "#eeeeee" }}>
         <NavbarToggler onClick={this.toggleNavbar} className="ml-2" />
         <Collapse navbar isOpen={!this.state.collapsed}>
-          <Nav navbar style={{ margin: "auto" }}>
+          <Nav navbar className="mx-auto">
             {HEADER.map((info, index) => {
               let icon = <></>
               if (index === HEADER.length / 2) {
                 icon = (
-                  <Link to="/" className="navbar-brand mr-auto">
+                  <Link
+                    to="/"
+                    className="navbar-brand mx-auto"
+                    key={index + 10}
+                  >
                     <Media
                       src={logo}
-                      style={{ maxHeight: "3rem", margin: "auto" }}
+                      style={{
+                        maxHeight: this.state.maxLogoSize + "rem",
+                        margin: "auto",
+                      }}
                       key={index + 10}
                     />
                   </Link>
