837f80f2ef800d22c080627630c362bc2c7005e1
Esteban Mata <esteban1113001@gmail.com>
2019-10-12 16:08:01 -0600

    Feitec Logo

--- src/components/bienvenida/bienvenida.js
+++ src/components/bienvenida/bienvenida.js
@@ -1,50 +1,51 @@
 import React from "react"
 import { Col, Row } from "reactstrap"
+import Logo from "../../images/Icons/FeitecWhite.png"
 
-const Bienvenida = () => (
-  <div
-    style={{
-      backgroundColor: "#9AC42F",
-      height: "25rem",
-      display: "flex",
-      alignContent: "center",
-    }}
-  >
+const Bienvenida = () => {
+  const isMobile = typeof window !== "undefined" && window.innerWidth < 480
+  const imgStyle = isMobile
+    ? {
+        height: "inherit",
+        paddingTop: "1rem",
+      }
+    : {
+        width: "60%",
+        float: "right",
+        padding: 0,
+      }
+
+  const textStyle = isMobile
+    ? {
+        margin: "auto",
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center",
+        fontSize: "4rem",
+      }
+    : {
+        margin: "auto",
+        color: "white",
+        fontWeight: "bold",
+        fontSize: "5rem",
+      }
+  return (
     <Row
       style={{
-        width: "100%",
-        margin: "auto",
+        backgroundColor: "#9AC42F",
+        minHeight: "25rem",
+        display: "flex",
+        alignContent: "center",
       }}
     >
-      <Col xs="8" sm="8" md="8" lg="8" xl="8" className="mx-auto">
-        <Col
-          xs="5"
-          sm="5"
-          md="5"
-          lg="5"
-          xl="5"
-          style={{ display: "inline-block" }}
-        >
-          a
-        </Col>
-        <Col
-          xs="7"
-          sm="7"
-          md="7"
-          lg="7"
-          xl="7"
-          style={{
-            display: "inline-block",
-            fontSize: "5rem",
-            color: "white",
-            fontWeight: "bold",
-          }}
-        >
-          Bienvenidos
-        </Col>
+      <Col xs="12" sm="12" md="5" lg="5" xl="5" style={{ margin: "auto" }}>
+        <img src={Logo} alt="Logo de Feitec" style={imgStyle} />
+      </Col>
+      <Col xs="11" sm="11" md="7" lg="7" xl="7" style={textStyle}>
+        Bienvenidos
       </Col>
     </Row>
-  </div>
-)
+  )
+}
 
 export default Bienvenida
